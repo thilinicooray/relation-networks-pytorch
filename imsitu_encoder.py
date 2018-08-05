c6b6b58fa0c1dee6fcf99decd4b4cccf9ba073fa
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-08-05 19:51:46 +0800

    change eval

--- imsitu_encoder.py
+++ imsitu_encoder.py
@@ -17,8 +17,6 @@ class imsitu_encoder():
         self.label_list = ['#UNK#']
         label_frequency = {}
         self.max_role_count = 0
-        self.role2_label = {}
-        self.role2_verb = {}
 
         for img_id in train_set:
             img = train_set[img_id]
@@ -35,20 +33,12 @@ class imsitu_encoder():
                         self.verb2_role_dict[current_verb].append(role)
                     if len(self.verb2_role_dict[current_verb]) > self.max_role_count:
                         self.max_role_count = len(self.verb2_role_dict[current_verb])
-                    if role not in self.role2_label:
-                        self.role2_label[role] = []
-                    if label not in self.role2_label[role]:
-                        self.role2_label[role].append(label)
-                    if role not in self.role2_verb:
-                        self.role2_verb[role] = []
-                    if current_verb not in self.role2_verb[role]:
-                        self.role2_verb[role].append(current_verb)
                     if label not in self.label_list:
                         if label not in label_frequency:
                             label_frequency[label] = 1
                         else:
                             label_frequency[label] += 1
-                        #only labels occur at least 5 times are considered
+                        #only labels occur at least 20 times are considered
                         if label_frequency[label] == 20:
                             self.label_list.append(label)
 
@@ -56,64 +46,40 @@ class imsitu_encoder():
               '\n\t label count:', len(self.label_list) ,
               '\n\t max role count:', self.max_role_count)
 
-        #self.save_stat(self.role2_label, 'role2label')
-        #self.save_stat(self.role2_verb, 'role2verb')
 
-        self.verb_embedding = torch.squeeze(torch.eye(len(self.verb_list)).view(len(self.verb_list),len(self.verb_list),1))
-        self.role_embedding = torch.squeeze(torch.eye(len(self.role_list)).view(len(self.role_list),len(self.role_list),1))
-        self.label_embedding = torch.squeeze(torch.eye(len(self.label_list)).view(len(self.label_list),len(self.label_list),1))
-
-        '''print('embedding sizes for verb, role and label ', self.verb_embedding.size(), self.role_embedding.size(),
-              self.label_embedding.size() )'''
-
-        verb2role_embedding_list = []
+        verb2role_list = []
         for verb_id in range(len(self.verb_list)):
             current_role_list = self.verb2_role_dict[self.verb_list[verb_id]]
 
-            role_embedding_verb = []
+            role_verb = []
             for role in current_role_list:
                 role_id = self.role_list.index(role)
-                embedding = self.role_embedding[role_id]
-                role_embedding_verb.append(embedding)
+                role_verb.append(role_id)
 
             padding_count = self.max_role_count - len(current_role_list)
 
             for i in range(padding_count):
-                role_embedding_verb.append(torch.zeros(len(self.role_list)))
+                role_verb.append(len(self.role_list))
 
-            verb2role_embedding_list.append(torch.stack(role_embedding_verb))
+            verb2role_list.append(torch.tensor(role_verb))
 
-        self.verb2role_embedding = torch.stack(verb2role_embedding_list)
+        self.verb2role_list = torch.stack(verb2role_list)
+        '''print('verb to role list :', self.verb2role_list.size())
 
-        '''print('Final check')
-        rand_verb = random.choice(self.verb_list)
-        rand_verb_id = self.verb_list.index(rand_verb)
-        print('selected verb and id :', rand_verb, rand_verb_id)
-        print('verb embedding \n', self.verb_embedding[rand_verb_id].size())
-        print(self.verb_embedding[rand_verb_id].t())
-        print('role details : \n')
-        for role in verb2_role_dict[rand_verb]:
-            print(role, self.role_list.index(role))
-            idx = verb2_role_dict[rand_verb].index(role)
-            print('role embedding \n', self.verb2role_embedding[rand_verb_id][idx].t())'''
+        print('unit test verb and roles: \n')
+        verb_test = [4,57,367]
+        for verb_id in verb_test:
+            print('verb :', self.verb_list[verb_id])
 
+            role_list = self.verb2role_list[verb_id]
 
-    '''def save_stat(self, dict, filename):
-        newdict = {}
-        for y in dict.items():
-            newdict[y[0]] = len(y[1])
-
-        d_sorted_by_value = OrderedDict(sorted(newdict.items(), key=lambda x: x[1], reverse=True))
-
-        import pandas as pd
-
-        (pd.DataFrame.from_dict(data=d_sorted_by_value, orient='index')
-         .to_csv(filename+'.csv', header=False))'''
-
+            for role in role_list:
+                if role != len(self.role_list):
+                    print('role : ', self.role_list[role])'''
 
     def encode(self, item):
         verb = self.verb_list.index(item['verb'])
-        roles = self.get_role_ids(item['verb'])
+        roles = self.get_role_ids(verb)
         labels = self.get_label_ids(item['frames'])
 
         #print('item encoding size : v r l', verb.size(), roles.size(), labels.size())
@@ -138,69 +104,21 @@ class imsitu_encoder():
     def get_num_labels(self):
         return len(self.label_list)
 
-    def get_verb_encoding(self, verb_id):
-        verbs = []
-
-        for id in verb_id:
-            verb = self.verb_embedding[id]
-            verbs.append(verb)
-
-        return torch.stack(verbs)
-
-    def get_role_encoding(self, verb_id):
-        verb_roles = []
-
-        for id in verb_id:
-            verb_r = self.verb2role_embedding[id]
-            verb_roles.append(verb_r)
-
-        return torch.stack(verb_roles)
-
     def get_role_count(self, verb_id):
         return len(self.verb2_role_dict[self.verb_list[verb_id]])
 
-    def get_adj_matrix(self, verb_encoding):
-        verb_ids = verb_encoding
-        adj_matrix_list = []
+    def get_role_ids_batch(self, verbs):
+        role_batch_list = []
 
-        for id in verb_ids:
-            adj = torch.zeros([self.max_role_count, self.max_role_count])
-            actual_verb_count = self.get_role_count(id)
-            adj[:actual_verb_count, : actual_verb_count] = 1
-            adj_matrix_list.append(adj)
+        for verb_id in verbs:
+            role_ids = self.get_role_ids(verb_id)
+            role_batch_list.append(role_ids)
 
-        return torch.stack(adj_matrix_list)
+        return torch.tensor(role_batch_list)
 
-    def apply_mask(self, roles, val):
-        mask = val.clone().fill_(1)
-        batch_size = roles.size(0)
-        for i in range(batch_size):
-            for j in range(self.get_max_role_count()):
-                role = roles[i][j]
-                if role == 190:
-                    #embedding[i][j] = embedding[i][j].fill_(0)
-                    mask[i,j:] = 0
-                    break
+    def get_role_ids(self, verb_id):
 
-        return mask
-
-    def get_role_ids(self, verb):
-        #print('verb', verb)
-        roles = self.verb2role_embedding[self.verb_list.index(verb)]
-        #print('roles', roles)
-        role_id = []
-
-        for role in roles:
-            val = role[role > 0]
-            if len(val) > 0:
-                _, id = torch.max(torch.unsqueeze(role,1), 0)
-                role_id.append(id)
-
-        pad = self.max_role_count - len(role_id)
-        for j in range(pad):
-            role_id.append(self.get_num_roles())
-
-        return torch.tensor(role_id)
+        return self.verb2role_list[verb_id]
 
     def get_label_ids(self, frames):
         all_frame_id_list = []
--- model_updated_big.py
+++ model_updated_big.py
@@ -41,6 +41,7 @@ class RelationNetworks(nn.Module):
     def __init__(
             self,
             encoder,
+            gpu_mode,
             conv_hidden=24,
             embed_hidden=512,
             lstm_hidden=512,
@@ -66,6 +67,7 @@ class RelationNetworks(nn.Module):
         ])
 
         self.encoder = encoder
+        self.gpu_mode = gpu_mode
         self.n_roles = self.encoder.get_num_roles()
         self.n_verbs = self.encoder.get_num_verbs()
         self.vocab_size = self.encoder.get_num_labels()
@@ -153,7 +155,7 @@ class RelationNetworks(nn.Module):
         qst = qst.repeat(1,n_pair * n_pair,1)
         qst = torch.squeeze(qst)
 
-        #print('qst size', qst.size())
+        print('qst size', qst.size())
 
         '''h_tile = role_verb_embd.permute(1, 0, 2).expand(
             batch_size_updated, n_pair * n_pair, self.lstm_hidden
@@ -162,7 +164,7 @@ class RelationNetworks(nn.Module):
 
         #update conv to expand for all roles in 1 image
         conv = conv.repeat(1,self.max_role_count, 1, 1)
-        #print('conv, size', conv.size())
+        print('conv, size', conv.size())
         conv = conv.view(-1, n_channel, conv_h, conv_w)
         #print('after view', conv.size())
         conv = torch.cat([conv, self.coords.expand(batch_size_updated, 2, conv_h, conv_w)], 1)
@@ -173,16 +175,96 @@ class RelationNetworks(nn.Module):
         conv1 = conv1.contiguous().view(-1, n_pair * n_pair, n_channel)
         conv2 = conv2.contiguous().view(-1, n_pair * n_pair, n_channel)
         #print('size :', conv2.size())
+        print('no issue efore cat')
         concat_vec = torch.cat([conv1, conv2, qst], 2).view(-1, self.n_concat)
+        print('no issue after cat')
         g = self.g(concat_vec)
-        g = g.view(-1, n_pair * n_pair, self.mlp_hidden*4).sum(1).squeeze()
 
+        if self.gpu_mode >= 0:
+            torch.cuda.empty_cache()
+        print('no issue after g')
+        g = g.view(-1, n_pair * n_pair, self.mlp_hidden*4).sum(1).squeeze()
+        print('no issue after g view')
         f = self.f(g)
+        print('no issue after f')
 
         role_predict = f.contiguous().view(batch_size, -1, self.vocab_size)
         #print('ffffff', f.size())
 
         return verb_pred, role_predict
+
+    def forward_eval(self, image):
+
+        '''print('testing 123')
+        x = torch.tensor([[1, 2, 3],[4,5,6]])
+        print('original', x.size())
+        x = x.repeat(1,2)
+        print('xxxxxx', x, x.view(-1,3), x.size())'''
+        batch_size = image.size(0)
+        conv = self.conv(image)
+
+        #verb pred
+        verb_pred = self.verb(conv.view(-1, 7*7*self.conv.base_size()))
+
+        pred_list = []
+
+        for i in batch_size:
+            conv_i = conv[i].expand(self.n_verbs, conv.size(1), conv.size(2), conv.size(3))
+            verbs = torch.arange(self.n_verbs)
+            roles = self.encoder.get_role_ids_batch(verbs)
+            print('roles ', roles.size())
+
+            batch_size, n_channel, conv_h, conv_w = conv.size()
+            n_pair = conv_h * conv_w
+
+            verb_embd = self.verb_lookup(verbs)
+            role_embd = self.role_lookup(roles)
+
+            role_embed_reshaped = role_embd.transpose(0,1)
+            verb_embed_expand = verb_embd.expand(self.max_role_count, verb_embd.size(0), verb_embd.size(1))
+            role_verb_embd = verb_embed_expand * role_embed_reshaped
+            role_verb_embd =  role_verb_embd.transpose(0,1)
+            role_verb_embd = role_verb_embd.contiguous().view(-1, self.lstm_hidden)
+            #new batch size = batch_size*max_role
+            batch_size_updated = role_verb_embd.size(0)
+            #print('new', batch_size_updated, n_pair, role_verb_embd.size())
+
+            qst = torch.unsqueeze(role_verb_embd, 1)
+            qst = qst.repeat(1,n_pair * n_pair,1)
+            qst = torch.squeeze(qst)
+
+            #print('qst size', qst.size())
+
+            '''h_tile = role_verb_embd.permute(1, 0, 2).expand(
+                batch_size_updated, n_pair * n_pair, self.lstm_hidden
+            )'''
+
+
+            #update conv to expand for all roles in 1 image
+            conv_i = conv_i.repeat(1,self.max_role_count, 1, 1)
+            #print('conv, size', conv.size())
+            conv_i = conv_i.view(-1, n_channel, conv_h, conv_w)
+            #print('after view', conv.size())
+            conv_i = torch.cat([conv_i, self.coords.expand(batch_size_updated, 2, conv_h, conv_w)], 1)
+            n_channel += 2
+            conv_tr = conv_i.view(batch_size_updated, n_channel, -1).permute(0, 2, 1)
+            conv1 = conv_tr.unsqueeze(1).expand(batch_size_updated, n_pair, n_pair, n_channel)
+            conv2 = conv_tr.unsqueeze(2).expand(batch_size_updated, n_pair, n_pair, n_channel)
+            conv1 = conv1.contiguous().view(-1, n_pair * n_pair, n_channel)
+            conv2 = conv2.contiguous().view(-1, n_pair * n_pair, n_channel)
+            #print('size :', conv2.size())
+            concat_vec = torch.cat([conv1, conv2, qst], 2).view(-1, self.n_concat)
+            g = self.g(concat_vec)
+            if self.gpu_mode >= 0:
+                torch.cuda.empty_cache()
+            g = g.view(-1, n_pair * n_pair, self.mlp_hidden*4).sum(1).squeeze()
+            f = self.f(g)
+
+            role_predict = f.contiguous().view(batch_size, -1, self.vocab_size)
+            pred_list.append(role_predict)
+            #print('ffffff', f.size())
+
+        return verb_pred, torch.stack(pred_list,0)
 
     def calculate_loss(self, verb_pred, gt_verbs, role_label_pred, gt_labels):
 
