8a0dd0c33e98e8886767816e4244df225c48a944
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-08-10 21:11:53 +0800

    making model smaller

--- main_vsrl_small.py
+++ main_vsrl_small.py
@@ -69,7 +69,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
 
             loss.backward()
 
-            #torch.nn.utils.clip_grad_norm_(model.parameters(), clip_norm)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_norm)
 
 
             '''for param in filter(lambda p: p.requires_grad,model.parameters()):
@@ -123,7 +123,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
                 max_score = max(dev_score_list)
 
                 if max_score == dev_score_list[-1]:
-                    torch.save(model.state_dict(), model_dir + "/{0}_small.model".format(max_score))
+                    torch.save(model.state_dict(), model_dir + "/{0}_lr0001sched.model".format(max_score))
                     print ('New best model saved! {0}'.format(max_score))
 
                 #eval on the trainset
