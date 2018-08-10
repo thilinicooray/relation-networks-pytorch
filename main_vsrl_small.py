494d422eef118cc3ca686393c5aba33001e3261b
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-08-10 10:32:56 +0800

    making model smaller

--- main_vsrl_small.py
+++ main_vsrl_small.py
@@ -123,7 +123,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
                 max_score = max(dev_score_list)
 
                 if max_score == dev_score_list[-1]:
-                    torch.save(model.state_dict(), model_dir + "/{0}.model".format(max_score))
+                    torch.save(model.state_dict(), model_dir + "/{0}_small.model".format(max_score))
                     print ('New best model saved! {0}'.format(max_score))
 
                 #eval on the trainset
