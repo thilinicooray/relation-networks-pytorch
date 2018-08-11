f1f410847f1e1bddb61117decef091f3ccf18c9b
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-08-11 11:05:21 +0800

    making model smaller

--- model_vsrl_small.py
+++ model_vsrl_small.py
@@ -44,7 +44,7 @@ class RelationNetworks(nn.Module):
             conv_hidden=24,
             embed_hidden=300,
             lstm_hidden=300,
-            mlp_hidden=256
+            mlp_hidden=512
     ):
         super().__init__()
 
@@ -106,7 +106,7 @@ class RelationNetworks(nn.Module):
             nn.Linear(mlp_hidden, mlp_hidden*2),
             nn.ReLU(),
             nn.Dropout(),
-            nn.Linear(mlp_hidden*2, self.vocab_size),
+            nn.Linear(mlp_hidden, self.vocab_size),
         )
         self.conv_hidden = self.conv.base_size()
         self.lstm_hidden = lstm_hidden
